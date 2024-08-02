db0589e6dcd1b4f9322bc5be321da05f3be0943c
otomototomoni <tegurechafu150415@icloud.com>
2024-08-02 14:18:14 +0900

    MainActivity.kt PersonaEditScreen.kt この二つの画面を行き来するような画面遷移を行うことができた。ボタンによる画面遷移

--- app/src/main/java/com/example/personacreatorjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/personacreatorjetpackcompose/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.personacreatorjetpackcompose.ui.theme.PersonaCreatorJetpackComposeTheme
+import com.example.personacreatorjetpackcompose.ui.theme.PersonaEditScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +27,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             PersonaCreatorJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CustomMainActivity(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+//-----------------------------------Main画面のView
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CustomMainActivity(modifier:Modifier = Modifier){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen(navController) }
+        composable("personaEdit") { PersonaEditScreen(navController) }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavHostController) {
+    Column {
+        Text("New Persona")
+        Button(onClick = {
+            navController.navigate("personaEdit")
+        }) {
+            Text(text = "新しいペルソナ作成")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     PersonaCreatorJetpackComposeTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "main") {
+            composable("main") { MainScreen(navController) }
+            composable("personaEdit") { PersonaEditScreen(navController) }
+        }
+        MainScreen(navController)
     }
 }
---
+++ app/src/main/java/com/example/personacreatorjetpackcompose/ui/theme/PersonaEditScreen.kt
@@ -0,0 +1,33 @@
+package com.example.personacreatorjetpackcompose.ui.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.personacreatorjetpackcompose.MainScreen
+
+//-----------------------------------画面遷移の準備
+@Composable
+fun PersonaEditScreen(navController: NavHostController){
+    Column {
+        Text("PersonaEditScreen")
+        Button(onClick = {
+            navController.navigate("main")
+        }) {
+            Text(text = "保存")
+        }
+    }
+}
+
