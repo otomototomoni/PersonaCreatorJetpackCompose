2b39fd27d6b2d74fb9e08cbb0d62602eef366656
otomototomoni <tegurechafu150415@icloud.com>
2024-08-17 02:26:32 +0900

    MainScreen.kt EditTextを追加 実際にはOutLinedField あまり理解していない。

--- app/src/main/java/com/example/personacreatorjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/personacreatorjetpackcompose/MainScreen.kt
@@ -1,12 +1,18 @@
 package com.example.personacreatorjetpackcompose
 
+import android.widget.EditText
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,6 +21,7 @@ import androidx.navigation.NavHostController
 //----------------------------------------------メイン画面
 @Composable
 fun MainScreen(navController: NavHostController) {
+    val textState = remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -22,15 +29,19 @@ fun MainScreen(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("New Persona")
-        Row{
             Button(onClick = {
                 navController.navigate("personaEdit")
             }) {
                 Text(text = "新しいペルソナ作成")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "aiueo")
-            }
-        }
+        OutlinedTextField(
+            value = textState.value,
+            onValueChange = { newText ->
+                textState.value = newText
+            },
+            label = { Text("Enter your name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text("You entered: ${textState.value}")
     }
 }
