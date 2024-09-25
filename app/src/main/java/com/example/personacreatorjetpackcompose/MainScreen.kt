b66fc6faa11ba0698a25fa8116f105ff2fa8f8d6
otomototomoni <tegurechafu150415@icloud.com>
2024-09-25 15:54:21 +0900

    簡単な足し算をできるようにした。 コードを自分なりに少し綺麗に

--- app/src/main/java/com/example/personacreatorjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/personacreatorjetpackcompose/MainScreen.kt
@@ -1,12 +1,20 @@
 package com.example.personacreatorjetpackcompose
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,6 +26,11 @@ import java.security.AlgorithmParameters
 //----------------------------------------------メイン画面
 @Composable
 fun MainScreen(navController: NavHostController) {
+
+    val num1 = remember{ mutableStateOf("") }
+    val num2 = remember{ mutableStateOf("") }
+    val sum = remember{ mutableStateOf(0) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -31,15 +44,59 @@ fun MainScreen(navController: NavHostController) {
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            //ペルソナ作成ボタン
+            Button(
+                onClick = { navController.navigate("personaEdit") }
+            ) {
+                Text(
+                    text = "新しいペルソナ作成"
+                )
+            }
 
-            Button(onClick = {
-                navController.navigate("personaEdit")
-            }) {
-                Text(text = "新しいペルソナ作成")
+            //数字の入力
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                OutlinedTextField(
+                value = num1.value,
+                onValueChange = {num1.value = it},
+                label = { Text("1つ目の数字") },
+                    modifier = Modifier
+                        .width(100.dp)
+                        .weight(1f)
+                )
+                //間隔
+                Spacer(
+                    modifier = Modifier
+                        .width(8.dp)
+                )
+                OutlinedTextField(
+                    value = num2.value,
+                    onValueChange = {num2.value = it},
+                    label = { Text("2つ目の数字") },
+                    modifier = Modifier
+                        .width(100.dp)
+                        .weight(1f)
+                )
+
+            }//Row
+
+            Button(
+                onClick = { sum.value = (num1.value.toIntOrNull()?:0) + (num2.value.toIntOrNull()?:0) },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text(
+                    text = "足し算",
+                )
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "aiueo")
-            }
+            Text(
+                "計算結果 --> ${sum.value} ",
+                modifier = Modifier.padding(top = 6.dp)
+            )
         }
     }
 }
