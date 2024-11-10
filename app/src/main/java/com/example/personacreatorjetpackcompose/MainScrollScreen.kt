04831c370b32fdc9d2a8be0bfec426babb1d1f00
otomototomoni <tegurechafu150415@icloud.com>
2024-11-11 03:02:11 +0900

    PersonaEditScreen.kt MainScrollScreen.kt 上記の二つのデザインを大きく変更した。 基本的にboxを使用したデザインで、クリックしたときの処理も加えた。

--- app/src/main/java/com/example/personacreatorjetpackcompose/MainScrollScreen.kt
+++ app/src/main/java/com/example/personacreatorjetpackcompose/MainScrollScreen.kt
@@ -1,6 +1,7 @@
 package com.example.personacreatorjetpackcompose
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -34,6 +36,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.sp
 import com.google.firebase.database.core.Tag
 
 /*
@@ -49,9 +52,6 @@ fun MainScrollScreen(navController: NavHostController,viewModel:MainViewModel,ov
     //データベースの情報を取得(仮)
     val userpersonas = viewModel.db.collection("${viewModel.auth.currentUser!!.uid}").document("Persona")//collectionとdocumentを指定
     var dbpersonas by remember { mutableStateOf<List<String>>(emptyList())}//データベースに入っているペルソナを取得する用
-    var personaname by remember { mutableStateOf("")}
-
-    personaname = "satou"
 
     Box(
         modifier = Modifier
@@ -73,28 +73,115 @@ fun MainScrollScreen(navController: NavHostController,viewModel:MainViewModel,ov
             .verticalScroll(scrollState)//垂直スクロールを有効にしている
 
     ) {
-        Column{
-            LaunchedEffect(Unit) {
-                //テスト：userに入っているメールアドレスを取得し、標示する。
+        Column {
+            //LaunchedEffect() {//メモ：データベースの変更があった際に更新できるようにする。snapshotStateOfを使うらしい
+                //ログインしているユーザーが登録しているペルソナの名前を取得
                 userpersonas.get()
                     .addOnSuccessListener { document ->
                         //dbpersonasにnameフィールドに入っている値を入れる
                         dbpersonas = document.get("name") as? List<String> ?: emptyList()
                     }
+                //ペルソナの数だけボタンを表示する。
+                dbpersonas.forEach { persona ->
+                    PersonaBox(navController,persona)
+//                    Button(
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(bottom = 10.dp),
+//                        onClick = {
+//                            navController.navigate("personaedit/${persona}")//引数の渡し方がおかしい
+//                        }
+//                    ) {
+//                        Text(text = persona)
+//                    }
                 }
-            //ペルソナの数だけボタンを表示する。
-            dbpersonas.forEach { persona ->
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 10.dp),
-                    onClick = {
-                        navController.navigate("personaedit/${persona}")//引数の渡し方がおかしい
-                    }
-                ){
-                    Text(text = persona)
-                }
-            }
+            //}
         }//column-end
     }//boc-end
 }//function-end
+
+//----------------------------------------------------------------------------------
+
+/*
+ペルソナの一覧を標示するときの一つ一つのデザイン
+ */
+@Composable
+fun PersonaBox(navController: NavHostController,persona : String){
+
+    val color = Color.Black
+    val lighterColor = color.copy(alpha = 0.5f)
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(8.dp)
+            )
+        //クリックしたときの処理
+            .clickable {
+                navController.navigate("personaedit/${persona}")
+            }
+    ){
+        //--------------------------------------------------------------------
+        //画像の右に名前と備考欄をつける。
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+
+            //イメージ写真の資格を描画。一番左に置く
+            Box(
+                modifier = Modifier
+                    .padding(end = 10.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .size(width = 100.dp, height = 100.dp)
+            ) {
+                Text(
+                    text = "NoImage",
+                    color = lighterColor,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            ){
+                //ペルソナの名前を表示
+                Text(
+                    text = persona,
+                    fontSize = 25.sp,
+                    modifier = Modifier
+                        .padding(bottom = 10.dp)
+                )
+                //備考欄
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                ){
+                    Text(
+                        text = "備考欄",
+                        color = lighterColor,
+                        modifier = Modifier
+                            .padding(10.dp)
+                    )
+                }
+            }
+        }//Column_End--------------------------------------------------------
+    }//一番外枠のBox_End
+}//function_End
--- app/src/main/java/com/example/personacreatorjetpackcompose/PesonaEditScreen.kt
+++ app/src/main/java/com/example/personacreatorjetpackcompose/PesonaEditScreen.kt
@@ -1,36 +1,166 @@
 package com.example.personacreatorjetpackcompose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 /*
     それぞれのペルソナの編集画面
+    上半分で地理変数、人口動態変数、心理的変数、行動変数をナビゲーションメニューでそれぞれ表示させる。
+    下半分でそれをまとめたペルソナを編集できるようにする
+        -----→写真や年齢などをまとめる。
  */
 
+//personaVariableを使用してfirebaseのフィールドなりなんなりを引っ張り出す。
+data class Item(val personaVariable : String, val text : String, val color : Color)
+
 @Composable
-fun PersonaEditScreen(navController: NavHostController,viewModel:MainViewModel,personaName:String){
+fun PersonaEditScreen(
+    navController: NavHostController,
+    viewModel: MainViewModel,
+    personaName: String
+) {
+
+    var backGroundColor by remember { mutableStateOf(Color.Yellow) }//付箋がクリックされたときに変更する
+
+    val stickNoteHeight = 80.dp//付箋の大きさ
+    val stickNoteWidth = 80.dp//付箋の大きさ
+
+    val items = listOf(
+        Item("Geographic", "地理変数", Color.Yellow),
+        Item("Demographic", "人口動態変数", Color.Red),
+        Item("Psychographic", "心理的変数", Color.Blue),
+        Item("Behavioral", "行動変数", Color.Green)
+    )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = personaName)
+            .background(color = backGroundColor)//背景色が変更される
+    ) {
+        //-----------------------------------------------------------------上半分
+        BoxWithConstraints {
+            val boxMaxWidth = maxWidth
+            val boxMaxHeight = maxHeight
 
-        Button(
-            onClick = { navController.navigate("main") }
-        ){
-            Text("戻る")
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(boxMaxHeight / 2)//画面全体の1/2を変数を木尾述するところにする
+            ) {
+
+                //ーーーーーーーーーーーーーーーー上の付箋部分
+                LazyRow() {
+                    items(items){item ->
+
+                        Box(
+                            modifier = Modifier
+                                //boxを曲線にする
+                                .clip(
+                                    RoundedCornerShape
+                                        (
+                                        topStart = 16.dp,
+                                        topEnd = 16.dp,
+                                        )
+                                )
+                                .background(item.color)
+                                .size(width = stickNoteWidth, height = stickNoteHeight)
+                                .border(
+                                    width = 1.dp,
+                                    color = Color.Black,
+                                    shape = RoundedCornerShape(
+                                        topStart = 16.dp,
+                                        topEnd = 16.dp
+                                    )
+                                )
+                                //クリックされたときの処理
+                                .clickable {
+                                    backGroundColor = item.color
+                                }
+                        ) {
+                            Text(text = item.text)
+                        }
+
+                    }
+
+                }//LazyRow_End
+
+                //地理的変数などの変数を入れるところーーーー
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                ) {
+                    // ベースとなるBoxの内容
+                    Text(text = personaName)
+                }
+
+            }//Column_End
+
+        }//BoxWithConstraints_End
+
+        //-----------------------------------------------------------------下半分
+        BoxWithConstraints {
+            val boxMaxWidth = maxWidth
+            val boxMaxHeight = maxHeight
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(boxMaxHeight / 4 * 3)
+                    .width(boxMaxWidth)
+                    .background(Color.White)
+            ) {
+
+                Box(
+                    modifier = Modifier
+                        .background(Color.White)
+                ) {
+                    Text(text = "下半分")
+                }
+
+            }//Column_End
+
+        }//BoxWithConstraints_End
+
+        Box() {
+            Button(
+                onClick = { navController.navigate("main") },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)//alignはboxの中でだけ生息している。
+            ) {
+                Text("戻る")
+            }
         }
-    }
 
+    }//全体のcolumnの最後の}---------------------------------------------------------------
 }
+
