9c4793bbf9261e0b2a8c59746a3f2e2e6d14852f
otomototomoni <tegurechafu150415@icloud.com>
2025-01-09 12:26:52 +0900

    inisialize commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    //DataStoreの依存関係の追加
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     //semantictのtagでエラーが発生するため
     implementation("androidx.compose.material3:material3:1.3.1")
 }
